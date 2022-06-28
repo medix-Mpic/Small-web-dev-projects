9ae7d08e67825a8fb153e77c2c45b0296ccd8c53
medix-Mpic <67704199+medix-Mpic@users.noreply.github.com>
2022-06-28 09:33:35 +0100

    Add files via upload

---
+++ PROJ 4/script.js
@@ -0,0 +1,8 @@
+const but = document.querySelector('.btn')
+const input = document.querySelector('.input')
+const search = document.querySelector('.search')
+
+but.addEventListener(('click'),()=>{
+    search.classList.toggle('active')
+    
+})
